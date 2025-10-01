9c9a941e1c60fd213e3d4271af73e059aa3a10e4
Vincent Devillers <vincent.devillers@layer4.fr>
2025-10-01 21:38:22 +0200

    Fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="/docs/projects">
+                        to="https://varga.foundation/#/projects">
                         Explorer nos projets 🚀
                     </Link>
                     <Link
